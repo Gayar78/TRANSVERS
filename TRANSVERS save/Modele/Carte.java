31d7c17af74c4bb457d7a83e789bf51c85323e70
Gayar78 <remi.thibault@outlook.fr>
2023-05-26 22:08:06 +0200

    ce que a fait milad

--- TRANSVERS save/Modele/Carte.java
+++ TRANSVERS save/Modele/Carte.java
@@ -3,32 +3,45 @@ package Modele;
 import java.util.ArrayList;
 
 public class Carte extends Boite {
-	public ArrayList<String> ListeStatusCarte = new ArrayList<String>();
-	public String StatusCarte;
-	
-	public Carte(String nom) {
-		super(nom);
-		this.ListeStatusCarte.add("Rien");
-		this.ListeStatusCarte.add("Vert");
-		this.ListeStatusCarte.add("Orange");
-		this.ListeStatusCarte.add("Rouge");
-		this.StatusCarte = ListeStatusCarte.get(0);
-	}
+    public ArrayList<String> ListeStatusCarte = new ArrayList<String>();
+    public String StatusCarte;
+    public String TexteCarte = "";
+    
+    public Carte(String nom) {
+        super(nom);
+        this.ListeStatusCarte.add("Rien");
+        this.ListeStatusCarte.add("Vert");
+        this.ListeStatusCarte.add("Orange");
+        this.ListeStatusCarte.add("Rouge");
+        this.StatusCarte = ListeStatusCarte.get(0);
+    }
 
-	public String getStatusCarte() {
-		return StatusCarte;
-	}
+    public String getStatusCarte() {
+        return StatusCarte;
+    }
 
-	public void setStatusCarte(int position) {
-		StatusCarte = ListeStatusCarte.get(position);
-	}
+    public void setStatusCarte(int position) {
+        StatusCarte = ListeStatusCarte.get(position);
+    }
 
-	public void texteCarte() {
-		// TODO - implement Carte.renseignerCarte
-		throw new UnsupportedOperationException();
-	}
-	
-	public void supprimerCarte() {
-		finalize();
-	}
+    public void setTexteCarte(String text) {
+        this.TexteCarte = text;
+    }
+    
+    public String getTexteCarte() {
+        if(TexteCarte.isEmpty()) {
+            String contenu = "vide";
+            return contenu;
+        }
+        return TexteCarte;
+    }
+    
+    public void supprimerTexteCarte() {
+        this.TexteCarte = "";
+        
+    }
+    
+    public void supprimerCarte() {
+        finalize();
+    }
 }
